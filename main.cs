a024cf5fb0a1231520e7fbd31ee32f3a54514995
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-26 01:18:23 +0000

    whitespace

--- main.cs
+++ main.cs
@@ -8,27 +8,27 @@ class Program
 {
     public static void Main(string[] args)
     {
-        
-        // This function states user's address input.
+
+        // This function states user's address input. 
         int userValue;
         Random rnd = new Random();
-        int randomNumber = rnd.Next(1,6);
-        
-        //Input
+        int randomNumber = rnd.Next(1, 6);
+
+        //Input 
         Console.WriteLine("This program is a guessing game.");
         Console.WriteLine("");
         Console.Write("Enter a number from 1-6: ");
         userValue = Convert.ToInt32(Console.ReadLine());
-        
+
         //Process
-        if ( userValue == randomNumber ) {
+        if (userValue == randomNumber){
             Console.WriteLine("Congratulations!");
-        } 
-        
-        if ( userValue != randomNumber ) {
+        }
+
+        if (userValue != randomNumber){
             Console.WriteLine("Oops! The number was " + randomNumber + ".");
         }
-        
+
         Console.WriteLine("\nDone.");
     }
 }
