15f54a77dd4a0450f91267992bf22e1cddc00f69
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-26 01:10:14 +0000

    indenting

--- main.cs
+++ main.cs
@@ -2,33 +2,33 @@
 // Created on: Mar 2022
 //
 // This program does basic math
-
 using System;
 
 class Program
 {
-  public static void Main(string[] args)
-  {
-    // This function states user's address input.
-    int userValue;
-    Random rnd = new Random();
-    int randomNumber = rnd.Next(1,6);
-    
-    //Input
-    Console.WriteLine("This program is a guessing game.");
-    Console.WriteLine("");
-    Console.Write("Enter a number from 1-6: ");
-    userValue = Convert.ToInt32(Console.ReadLine());
-    
-    //Process
-    if ( userValue == randomNumber ) {
-      Console.WriteLine("Congratulations!");
-    } 
-    
-    if ( userValue != randomNumber ) {
-      Console.WriteLine("Oops! The number was " + randomNumber + ".");
+    public static void Main(string[] args)
+    {
+        
+        // This function states user's address input.
+        int userValue;
+        Random rnd = new Random();
+        int randomNumber = rnd.Next(1,6);
+        
+        //Input
+        Console.WriteLine("This program is a guessing game.");
+        Console.WriteLine("");
+        Console.Write("Enter a number from 1-6: ");
+        userValue = Convert.ToInt32(Console.ReadLine());
+        
+        //Process
+        if ( userValue == randomNumber ) {
+            Console.WriteLine("Congratulations!");
+        } 
+        
+        if ( userValue != randomNumber ) {
+            Console.WriteLine("Oops! The number was " + randomNumber + ".");
+        }
+        
+        Console.WriteLine("\nDone.");
     }
-    
-    Console.WriteLine("\nDone.");
-  }
 }
